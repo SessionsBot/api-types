28ab22e95b61217cd6f6418fef1883a7226017ba
br4dyb <halting.credo.00@icloud.com>
2025-07-31 00:56:41 -0500

    fix: remove `dist` from git ignore

---
+++ dist/firestoreData.types.d.ts
@@ -0,0 +1,71 @@
+/** Full Firebase document data saved internally for guild, if any. */
+export interface FirebaseGuildDoc {
+    /** Accent color used within guild. */
+    accentColor?: string;
+    /** Array of guild role ids granted admin permissions. */
+    adminRoleIds?: any[];
+    /** All session configurations currently scheduled for guild. */
+    sessionSchedules?: SessionSchedule[];
+    /** Current signup/panel post configuration for guild. */
+    sessionSignup?: SessionSignup;
+    /** Current timezone configuration for guild. */
+    timeZone?: string;
+    /** Indication whether guild has successfully completed setup process. */
+    setupCompleted?: boolean;
+    /** Key/Value pairs of upcoming sessions ids and their according data. */
+    upcomingSessions?: {
+        [key: string]: UpcomingSession;
+    };
+}
+export interface SessionSignup {
+    /** Guild text channel id used for posting new signup panels. */
+    panelChannelId?: string;
+    /** Daily time chosen for signup panels to be posted. */
+    dailySignupPostTime?: SessionSignupDailySignupPostTime;
+    /** Array of guild role ids to mention on new signup panel post. */
+    mentionRoleIds?: string[];
+}
+export interface SessionSchedule {
+    /** Title of session. */
+    sessionTitle?: string;
+    /** Date of session. */
+    sessionDateDaily?: Date;
+    /** Location of session. */
+    sessionUrl?: string;
+    /** Roles of session. */
+    roles?: SessionRole[];
+}
+export interface UpcomingSession {
+    /** Date of session. */
+    date?: UpcomingSessionDate;
+    /** Roles of session. */
+    roles?: SessionRole[];
+    /** Title of session. */
+    title?: string;
+    /** Location of session. */
+    location?: string;
+    /** Message Id of *Signup Panel* for session. */
+    signupPanelMsgId?: string;
+}
+export interface UpcomingSessionDate {
+    /** Hour time of session. */
+    hours?: number;
+    /** Minute time of session. */
+    minutes?: number;
+    /** Discord timestamp of session. */
+    discordTimestamp?: string;
+}
+export interface SessionRole {
+    /** Name of session role. */
+    roleName?: string;
+    /** Description of session role. */
+    roleDescription?: string;
+    /** Emoji for session role. */
+    roleEmoji?: string;
+    /** Capacity of session role. */
+    roleCapacity?: number;
+}
+export interface SessionSignupDailySignupPostTime {
+    hours?: number;
+    minutes?: number;
+}
---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export * from './backendApi.types';
+export { GuildGeneralData, GuildChannelData } from './discordApi.types';
+export { FirebaseGuildDoc } from './firestoreData.types';
